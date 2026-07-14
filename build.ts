897e8849e0f74f4c011bd2cd33de7e7c18726a6a
aidan <66340033+aid7n@users.noreply.github.com>
2026-07-14 19:54:03 +0100

    chore: update build process

--- build.ts
+++ build.ts
@@ -1,7 +1,13 @@
+import packageJson from "./package.json" with { type: "json" };
+
+const version = packageJson.version;
+const target = process.argv[2] as Bun.Build.CompileTarget | undefined;
+
 await Bun.build({
   entrypoints: ["./src/index.ts"],
   target: "bun",
   compile: {
-    outfile: "./dist/yarn",
+    target,
+    outfile: `./dist/${target ? version + "-" + target.replace("bun-", "") + "/yarn" : "yarn"}`,
   },
 }).then((result) => console.log(result));
